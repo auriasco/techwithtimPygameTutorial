7ed03773ae4f4b230abefdae846b23569ea96212
auacordero <auacordero@gmail.com>
2023-02-25 15:38:30 -0600

    started maze function

--- game3.py
+++ game3.py
@@ -1,19 +1,35 @@
 import pygame
 import math as m
 import pygame.draw as d
+import random as r
 pygame.init()
 
 def create_sprite(self):
-    h_width = 20 
-    b_width = 50
-    pygame.draw.circle(self,(255,0,0),(x-20,y+100),h_width)
-    pygame.draw.rect(self,(255,0,0),(x-45,y+120,b_width,b_width+25))
+    scaling_f = 2
+    h_width = 4*scaling_f
+    b_width = 10*scaling_f
+    b_length = b_width+5*scaling_f
+    pygame.draw.circle(self,(255,0,0),(x-4*scaling_f,y+20*scaling_f),h_width)
+    pygame.draw.rect(self,(255,0,0),(x-9*scaling_f,y+24*scaling_f,b_width,b_length))
+def bottom_top(self):
+    scaling_f = 2
+    block_length = 5*scaling_f
+    pygame.draw.rect(self,(255,0,0),(0,0,500,50))
+    pygame.draw.rect(self,(255,0,0),(0,450,500,50))
+def row(self,n):
+    #row_length = r.randint(10,50)
+    pygame.draw.rect(self,(255,0,0),(0,150,n,25))
+    pygame.draw.rect(self,(0,0,255),(n,150,500-n,25))
+       
+
+def maze(self):
+    bottom_top(self)
+    row(self,100)
+    
 
 
-
-
-win2 = pygame.display.set_mode((500,500))
-pygame.display.set_caption("First Game")
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("MazeGame")
 
 x = 50
 y = 50
@@ -22,6 +38,8 @@ height = 60
 vel = 5
 
 run = True
+win.fill((0,0,0))
+
 while run:
     pygame.time.delay(100)
 
@@ -38,7 +56,7 @@ while run:
         y -= vel
     if keys[pygame.K_DOWN]:
         y += vel
-    win2.fill((0,0,0))
+    win.fill((0,0,0))
     
     #test_ellipse()
 
@@ -47,7 +65,9 @@ while run:
     #pygame.draw.ellipse(win2,(255,0,0),(x+100,y-100,width,height))
     #pygame.draw.circle(win2,(255,0,0),(x-20,y+100),20)
     #pygame.draw.rect(win2,(255,0,0),(x-45,y+120,50,50))
-    create_sprite(win2)
+    create_sprite(win)
+    maze(win)
     #pass window object to function to create sprite
     pygame.display.update()
+
 pygame.quit
