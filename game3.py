82588258a82ee73dcfa9ac9352ba0ee92fa96301
auacordero <auacordero@gmail.com>
2023-02-27 11:10:21 -0600

    added numpy array

--- game3.py
+++ game3.py
@@ -1,6 +1,6 @@
 import pygame
 import math as m
-import pygame.draw as d
+import numpy as np
 import random as r
 pygame.init()
 
@@ -20,19 +20,30 @@ def row1(self,n,i):
     #row_length = r.randint(10,50)
     pygame.draw.rect(self,(255,0,0),(0,i,n,25))
     pygame.draw.rect(self,(0,0,255),(n,i,500-n,25))
-def row2(self,n,i):
-    pygame.draw.rect(self,(255,0,0),(0,i,n,25))
-    pygame.draw.rect(self,(0,0,255),(n,i,n+250,25))   
-    pygame.draw.rect(self,(255,0,0),(n+250,i,500-(n+250),25))
+def row2(self,n,j):
+    pygame.draw.rect(self,(255,0,0),(0,j,n,25))
+    t = 0 # length of blue block, n is the starting position
+    if (j > 300 and j < 350):
+        t = 500-t
+    else:
+        t = n+25
+    pygame.draw.rect(self,(0,0,255),(n,j,t,25))   
+    pygame.draw.rect(self,(255,0,0),(t,j,500-t,25))
   
+def create_3dM(size):
+    return np.zeros((size,size,size)) 
 
 def maze(self):
     bottom_top(self)
+    create_3dM(500)
     #row1(self,100,275)
     #row1(self,100,300)
-    row2(self,10,300)
-    
-
+    for i in range(250,400):
+        #row2(self,10,i)
+        if(i > 300):
+            row2(self,10,i) #win, col, row
+        else:
+            row2(self,10,i)
 
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("MazeGame")
@@ -71,8 +82,9 @@ while run:
     #pygame.draw.ellipse(win2,(255,0,0),(x+100,y-100,width,height))
     #pygame.draw.circle(win2,(255,0,0),(x-20,y+100),20)
     #pygame.draw.rect(win2,(255,0,0),(x-45,y+120,50,50))
-    create_sprite(win)
+    
     maze(win)
+    create_sprite(win)
     #pass window object to function to create sprite
     pygame.display.update()
 
